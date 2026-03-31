23a63eb77cdf7209c919ab42eda230821fb7463a
Jordan <146021463+Jordan-Potts@users.noreply.github.com>
2026-03-31 14:13:58 +0100

    Change shutdown message to reboot and update command

--- osdcloud_config.ps1
+++ osdcloud_config.ps1
@@ -3,6 +3,6 @@ Start-Sleep -Seconds 5
 
 Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 25H2 -OSEdition Pro -OSLanguage en-gb -OSLicense Retail -ZTI
 
-Write-Host -ForegroundColor Yellow "Device will shut down in 20 seconds..."
+Write-Host -ForegroundColor Yellow "Device will reboot in 20 seconds..."
 Start-Sleep -Seconds 20
-wpeutil shutdown
+wpeutil reboot
