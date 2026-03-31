63f5a439d2ed4661a0f5cbea6548520ee6b7efdb
Jordan <146021463+Jordan-Potts@users.noreply.github.com>
2026-03-31 11:53:36 +0100

    Change reboot message to shutdown with delay

--- osdcloud_config.ps1
+++ osdcloud_config.ps1
@@ -3,6 +3,6 @@ Start-Sleep -Seconds 5
 
 Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 25H2 -OSEdition Pro -OSLanguage en-gb -OSLicense Retail -ZTI
 
-Write-Host -ForegroundColor Yellow "Installation complete - rebooting..."
-Start-Sleep -Seconds 10
-wpeutil reboot
+Write-Host -ForegroundColor Yellow "Device will shut down in 20 seconds..."
+Start-Sleep -Seconds 20
+wpeutil shutdown
